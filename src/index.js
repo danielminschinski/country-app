39d57bedbcf6e4d52cfd5d931a09ece25db24e6e
Daniel <danielminschinski@gmail.com>
2019-12-13 13:44:55 +0100

    edit index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
-import DevTools from './DevTools';
+import Navigation from './presentational/navigation.component';
+
 
 render(
 	<Provider store={store}>
-		<div>
-			<h1>Inicjalizacja projektu</h1>
-			<DevTools />
-		</div>
+		<Router history={hashHistory}>
+			<Route path='/' component={Navigation}>
+			
+			</Route>
+		</Router>
 	</Provider>,
 	document.getElementById('root')
 );
