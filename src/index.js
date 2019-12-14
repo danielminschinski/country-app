a4999c6186705ae974946c9123d22c39fb0786d0
Daniel <danielminschinski@gmail.com>
2019-12-14 08:18:08 +0100

    edit country app cz2

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
-import Navigation from './presentational/navigation.component';
+import routes from './routes';
 
 
 render(
 	<Provider store={store}>
-		<Router history={hashHistory}>
-			<Route path='/' component={Navigation}>
-			
-			</Route>
-		</Router>
+		<Router history={hashHistory} routes={routes}/>
 	</Provider>,
 	document.getElementById('root')
 );
