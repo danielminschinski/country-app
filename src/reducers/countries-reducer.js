9da68b592341b6c1acf6513e3e0e07292b7b58d5
Daniel Minschinski <danielminschinski@gmail.com>
2019-12-18 17:07:14 +0100

    edit countries-reducer

--- src/reducers/countries-reducer.js
+++ src/reducers/countries-reducer.js
@@ -24,8 +24,9 @@ const countriesReducer = function (state = initialState, action){
 		case SET_CONTINENT:
 			const continentCountries = state.countries.filter(country => country.continent === action.name);
 			return Object.assign({}, state, {visibleCountries: continentCountries});
+		default: 
+			return state;
 	}
-	return state;
 };
 
 export default countriesReducer;
